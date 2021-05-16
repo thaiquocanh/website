e4f26695862030ed9ba962b8b926253ca58a7920
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-05-16 10:19:53 +0700

    Rebuild site

---
+++ ktsthaiquocanh/build.sh
@@ -0,0 +1,5 @@
+cd /home/solus/Documents/ThaiQuocAnh/website/ktsthaiquocanh/
+hugo -t introduction && git add -A && git commit -m "Rebuild site" && git push
+cd /home/solus/Documents/ThaiQuocAnh/website/ktsthaiquocanh/public
+git add -A && git commit -m "Rebuild site" && git push
+cd /home/solus/Documents/ThaiQuocAnh/website/ktsthaiquocanh/
